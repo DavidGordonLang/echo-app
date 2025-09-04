9e1e7db5f80c3cf094e36b924da3334661448362
DavidGordonLang <davidgordonlang@gmail.com>
2025-09-04 14:42:27 +0100

    Create questions.js

---
+++ src/data/questions.js
@@ -0,0 +1,10 @@
+const questions = [
+  "What’s something you’ve lost that you’d give anything to feel again?",
+  "What’s something you want but don’t yet have—and what’s standing in the way?",
+  "When do you feel most like yourself?",
+  "What’s something you never say out loud, but think about often?",
+  "What do you most want from others, but rarely ask for?",
+  "What are you hoping Echo will help you with?"
+];
+
+export default questions;
